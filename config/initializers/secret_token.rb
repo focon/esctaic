6e2581651e0ffc11e8f86c165f6b2c203eecff09
focon <focon@xs4all.nl>
2012-02-06 22:57:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Esctaic::Application.config.secret_token = 'f47845945d4597b0eb3dd17f9e441bd5cef79454c58f6edf9bc7ab185489334424de132db8d314ef3baac1fa42b945f82a19b6073e4fdd5c507ebac4c9d57658'
