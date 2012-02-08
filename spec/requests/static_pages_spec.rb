d7ec686e908d18b5d5629d7267a13c041d6dd6be
focon <focon@xs4all.nl>
2012-02-08 20:35:27 +0100

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Esctaic'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Esctaic')
+    end
+
+    it "should have the title 'Esctaic Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Esctaic | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "Esctaic | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Esctaic'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Esctaic')
+    end
+
+    it "should have the title 'About Esctaic'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "Esctaic | About")
+    end
+  end
+end
